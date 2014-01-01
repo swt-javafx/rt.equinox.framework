ec5c14361bdc88a6e2e163b76c17dc2fcac63268
Doug Schaefer <dschaefer@qnx.com>
2014-01-01 16:54:35 -0500

    Add JavaFX class loading hook.

---
+++ bundles/org.eclipse.osgi.javafx/src/org/eclipse/osgi/javafx/JavaFXClassLoaderDelegateHook.java
@@ -0,0 +1,24 @@
+package org.eclipse.osgi.javafx;
+
+import org.eclipse.osgi.internal.hookregistry.ClassLoaderHook;
+import org.eclipse.osgi.internal.hookregistry.HookConfigurator;
+import org.eclipse.osgi.internal.hookregistry.HookRegistry;
+import org.eclipse.osgi.internal.loader.ModuleClassLoader;
+
+@SuppressWarnings("restriction")
+public class JavaFXClassLoaderDelegateHook extends ClassLoaderHook implements HookConfigurator {
+
+	@Override
+	public void addHooks(HookRegistry hookRegistry) {
+		hookRegistry.addClassLoaderHook(this);
+	}
+
+	@Override
+	public Class<?> postFindClass(String name, ModuleClassLoader classLoader) throws ClassNotFoundException {
+		if (name.startsWith("javafx."))
+			return ClassLoader.getSystemClassLoader().loadClass(name);
+		else
+			return null;
+	}
+
+}
